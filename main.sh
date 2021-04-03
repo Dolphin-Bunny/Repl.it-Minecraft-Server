bd7f15e3fb662f383217723808981575f2d930cc
Dolphin-Bunny <49925062+Dolphin-Bunny@users.noreply.github.com>
2021-04-03 10:28:38 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
-
 mkdir server
+clear
+if test ! -f "server/eula.txt"; then
+echo You must agree to the Minecraft EULA in order to proceed.
+echo https://account.mojang.com/documents/minecraft_eula
+echo Press enter to agree, ctrl+c to not agree. Pressing '"stop"' will also count as '"not agree"'
+read X
+echo "eula=true" >server/eula.txt
+clear
+fi
+
 if test ! -f "ngrok"; then
 rm -f ngrok-stable-linux-amd64.zip
 rm -f ngrok
@@ -13,6 +22,7 @@ if test ! -f "server/server.jar"; then
 wget -O "server/server.jar" "$mc_server_url"
 fi
 clear
-rm -rf logs
+rm -rf server/logs
 cd server
 java -jar server.jar nogui & python ../main.py & ../ngrok tcp 25565 -authtoken="$ngrok_token"
+#python test.py & ./ngrok http 8080 -authtoken="$ngrok_token"
